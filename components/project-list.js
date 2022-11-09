4ab14513d9dcb83e1fdac384d3a2bca604d4b67f
milanbargiel <milan.bargiel@googlemail.com>
2022-11-09 18:26:36 +0100

    Add desktop layout

--- components/project-list.js
+++ components/project-list.js
@@ -6,14 +6,15 @@ import kulturgeneratorPic from '../public/kulturgenerator-cologne-street.jpg'
 
 export default function ProjectList() {
   return (
-    <div className="px-3 md:p-0">
+    <div className="px-2 md:p-0">
       <Project
         roles={['Editorial Product Development']}
         title="A lightweight, scalable database that stores more than 10.000 comments each day"
         description="In 2020, the news website ZEIT ONLINE replaced its Drupal-based comments database with a lightweight, flexible and scalable system that stores more than 10.000 new comments each day. As a product owner, I set up an agile development process and defined the new database’s qualities in user stories."
+        wideTextBox={true}
         image={commentsPic}
         imageAlt="Comments database"
-        imageStyles="ml-12 drop-shadow-sm"
+        imageStyles="w-20 ml-12 drop-shadow-sm md:w-24 md:pt-12"
       ></Project>
       <Project
         roles={['Interface Design', 'Web Development']}
@@ -34,6 +35,7 @@ export default function ProjectList() {
         roles={['Product Design', 'Web Development']}
         title="Blueprint for a virtual art gallery in times of quarantine and isolation"
         description="The &#8220;kulturgenerator&#8221; is a generative redistribution machine, an experiment, and a virtual store that existed in the city of Cologne during the first year of the COVID pandemic."
+        wideTextBox={true}
         image={kulturgeneratorPic}
         imageAlt="Poster from the online art gallery in the streets of Cologne, Germany"
         imageStyles="ml-2 w-1/2 -rotate-6 rounded-md"
--- components/project.js
+++ components/project.js
@@ -5,19 +5,33 @@ export default function Project(props) {
   const roleItems = props.roles.map((role) => <li key={role.id}>{role}</li>)
 
   return (
-    <article className="px-3 pt-8 pb-8 mb-12 rounded-md bg-apricot md:px-8">
-      <ul className="text-xs mb-6 leading-normal">{roleItems}</ul>
-      <h3 className="text-2xl mb-8">{props.title}</h3>
-      <div className="text-sm mb-10">{props.description}</div>
-      <Image
-        className={
-          props.imageStyles
-            ? props.imageStyles
-            : 'rounded-md border border-gray-400'
-        }
-        src={props.image}
-        alt={props.imageAlt}
-      />
+    <article className="px-4 pt-8 pb-4 mb-12 rounded-md bg-apricot md:px-8 md:pb-8 md:pt-10 md:flex">
+      <div
+        className={`md:flex md:flex-col md:mr-8 ${
+          props.wideTextBox ? 'md:w-7/12' : 'md:w-1/2'
+        }`}
+      >
+        <ul className="text-sm mb-6 leading-normal md:order-last md:mb-0">
+          {roleItems}
+        </ul>
+        <h3 className="text-2xl leading-tight mb-8 md:w-4/5">{props.title}</h3>
+        <div className="mb-10">{props.description}</div>
+      </div>
+      <div
+        className={`md:flex md:items-center md:justify-center md:ml-4 ${
+          props.wideTextBox ? 'md:w-5/12' : 'md:w-1/2'
+        }`}
+      >
+        <Image
+          className={
+            props.imageStyles
+              ? props.imageStyles
+              : 'rounded-md border border-gray-400'
+          }
+          src={props.image}
+          alt={props.imageAlt}
+        />
+      </div>
     </article>
   )
 }
