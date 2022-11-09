4224868ccc549201824082fb2e6fcf0d4a6b5d9c
milanbargiel <milan.bargiel@googlemail.com>
2022-11-09 11:06:44 +0100

    Add basic project list component

--- components/project.js
+++ components/project.js
@@ -1,18 +1,13 @@
 import Image from 'next/image'
 
 export default function Project(props) {
+  console.log(props.image)
   return (
     <article className="px-4 pt-8 pb-4 bg-apricot">
       <div className="text-xs mb-4">{props.roles}</div>
       <h3 className="text-xl mb-4">{props.title}</h3>
       <div className="text-sm mb-8">{props.description}</div>
-      <Image
-        className="ml-12"
-        src={props.imageURL}
-        alt="Comments database"
-        width={94}
-        height={150}
-      />
+      <Image className="ml-12" src={props.image} alt={props.imageAlt} />
     </article>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 import Intro from '../components/intro'
-import Project from '../components/project'
+import ProjectList from '../components/project-list'
 
 export default function Home() {
   return (
     <div>
       <Intro></Intro>
-      <div className="px-2">
-        <Project
-          roles="Editorial Product Development"
-          title="A lightweight, scalable database that stores more than 10.000 comments each day"
-          description="In 2020, the news website ZEIT ONLINE replaced its Drupal-based comments database with a lightweight, flexible and scalable system that stores more than 10.000 new comments each day. As a product owner, I set up an agile development process and defined the new database’s qualities in user stories."
-          imageURL="/comments-database.svg"
-        ></Project>
-      </div>
+      <ProjectList></ProjectList>
     </div>
   )
 }
