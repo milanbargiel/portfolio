fd5e587559b25712b39092de49d86740571a30ac
milanbargiel <milan.bargiel@googlemail.com>
2022-11-09 11:22:44 +0100

    Process roles array for projects

--- components/project-list.js
+++ components/project-list.js
@@ -8,28 +8,28 @@ export default function ProjectList() {
   return (
     <div className="px-2">
       <Project
-        roles="Editorial Product Development"
+        roles={['Editorial Product Development']}
         title="A lightweight, scalable database that stores more than 10.000 comments each day"
         description="In 2020, the news website ZEIT ONLINE replaced its Drupal-based comments database with a lightweight, flexible and scalable system that stores more than 10.000 new comments each day. As a product owner, I set up an agile development process and defined the new database’s qualities in user stories."
         image={commentsPic}
         imageAlt="Comments database"
       ></Project>
       <Project
-        roles="Interface Design Web Development"
+        roles={['Interface Design', 'Web Development']}
         title="An archive of socio-ecological design patterns for architects, city developers and residents"
         description="Building Social Ecology showcases design patterns for socio-ecological buildings in Europe and shall serves as a source of inspiration for architects, project developers and residents."
         image={bsePic}
         imageAlt="Screenshot of buildingsocialecology.org"
       ></Project>
       <Project
-        roles="Editorial Product Development Usability Engineering"
+        roles={['Editorial Product Development', 'Usability Engineering']}
         title="A digital tool for the moderation of comments at ZEIT ONLINE"
         description="Comments are a crucial feature of the new web page. They enrich the discussion, are often complex and allow readers to engage with stories. The goal was to completely rebuild the old interface so that moderators have a delightful tool to work with."
         image={zocaPic}
         imageAlt="Example comments sections from ZEIT ONLINE"
       ></Project>
       <Project
-        roles="Product design Web development"
+        roles={['Product Design', 'Web Development']}
         title="Blueprint for a virtual art gallery in times of quarantine and isolation"
         description="The &#8220;kulturgenerator&#8221; is a generative redistribution machine, an experiment, and a virtual store that existed in the city of Cologne during the first year of the COVID pandemic."
         image={kulturgeneratorPic}
--- components/project.js
+++ components/project.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
 export default function Project(props) {
-  console.log(props.image)
+  // Create HTML li items from props.roles array
+  const roleItems = props.roles.map((role) => <li key={role.id}>{role}</li>)
+
   return (
     <article className="px-4 pt-8 pb-4 bg-apricot">
-      <div className="text-xs mb-4">{props.roles}</div>
+      <ul className="text-xs mb-4 leading-normal">{roleItems}</ul>
       <h3 className="text-xl mb-4">{props.title}</h3>
       <div className="text-sm mb-8">{props.description}</div>
       <Image className="ml-12" src={props.image} alt={props.imageAlt} />
