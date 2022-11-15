22c3a2817a78554e29d581c01f00e6b925f275dc
milanbargiel <milan.bargiel@googlemail.com>
2022-11-15 17:13:17 +0100

    Finetune desktop layout

--- components/intro-bold.js
+++ components/intro-bold.js
@@ -1,6 +1,6 @@
 export default function Intro() {
   return (
-    <div className="mx-auto max-w-2xl text-2xl leading-snug font-medium pt-12 pb-20 px-3 md:text-3xl md:text-center md:pt-20 md:pb-32 md:px-16">
+    <div className="mx-auto max-w-3xl text-2xl leading-snug font-medium pt-12 pb-20 px-3 md:text-3xl md:text-center md:pt-20 md:pb-32 md:px-24">
       Hi, my name is Milan Bargiel. <br className="hidden md:inline" />I am a
       product designer and web developer living and working in Berlin, former
       editorial product developer at&nbsp;
@@ -14,17 +14,6 @@ export default function Intro() {
       </a>
       . I am interested in UX design, research and building digital things. Find
       some selected projects below.
-      <span className="hidden md:inline">
-        {' '}
-        For inquiries contact:&nbsp;
-        <br />
-        <a
-          className="underline underline-offset-4 decoration-1 decoration-gray-700 hover:decoration-2 hover:transition-all hover:duration-150"
-          href="mailto:mail@milanbargiel.com"
-        >
-          milanbargiel@pm.me
-        </a>{' '}
-      </span>
     </div>
   )
 }
--- components/project-list.js
+++ components/project-list.js
@@ -12,7 +12,7 @@ export default function ProjectList() {
         title="A lightweight, scalable database that stores more than 10.000 comments a day"
         image={commentsPic}
         imageAlt="Comments database"
-        imageStyles="hidden md:inline-block md:w-28 md:ml-0 md:mr-12 md:mt-8 rotate-1 drop-shadow-apricot"
+        imageStyles="hidden md:inline-block md:w-24 md:ml-0 md:mr-12 md:mt-8 rotate-1 drop-shadow-apricot"
       >
         In 2020, the news website ZEIT ONLINE replaced its Drupal-based comments
         database with a scalable system that stores more than 10.000 new
