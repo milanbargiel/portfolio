9e0edd27baa460b8290000fca458420175627c76
milanbargiel <milan.bargiel@googlemail.com>
2022-11-15 09:24:55 +0100

    Update mobile intro layout

--- components/intro.js
+++ components/intro.js
@@ -1,20 +1,26 @@
-import Link from 'next/link'
+import Link from '../components/link'
 
 export default function Intro() {
   return (
     <div className="mx-6 my-12 md:mb-20 md:mx-8 md:w-2/3">
-      <Link href="/" className="inline-block mb-20">
-        <h1 className="font-bold">Milan Bargiel</h1>
+      <Link href="/" className="inline-block mb-24">
+        <h1 className="font-semibold">Milan Bargiel</h1>
         <span className="text-gray-500 mb-4">Product & web developer</span>
       </Link>
-      <div className="mb-14">
-        <h2 className="font-bold mb-4">About</h2>I am a product designer and web
-        developer living and working in Berlin, Germany. Former editorial
-        product developer at ZEIT ONLINE I am currently looking for a job. I am
-        interested in UX design, research and building digital things. Find some
-        selected projects below.
+      <div className="mb-12">
+        <h2 className="font-semibold mb-3">About</h2>I am a product designer and
+        web developer living and working in Berlin, Germany. Currently I am open
+        to work. Former editorial product developer at&nbsp;
+        <Link
+          variant="underline"
+          href="https://www.zeit.de/autoren/B/Milan_Bargiel/indexhttps://www.zeit.de/autoren/B/Milan_Bargiel/index"
+        >
+          ZEIT ONLINE
+        </Link>
+        . I am interested in UX design, research and building digital things.
+        Find some selected projects below.
       </div>
-      <h2 className="font-bold mb-4">Contact</h2>
+      <h2 className="font-semibold mb-3">Contact</h2>
       <div className="flex flex-col md:flex-row md:flex-wrap">
         <div className="text-gray-500 md:basis-1/4">Email</div>
         <div className="md:basis-3/4">
