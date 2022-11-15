3396cb83f32c18792fb711cba8a4b43de5c22c8f
milanbargiel <milan.bargiel@googlemail.com>
2022-11-15 10:58:49 +0100

    Fine tune mobile version of imprint page

--- pages/legal-notice.js
+++ pages/legal-notice.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 export default function LegalNotice() {
   return (
     <div className="text-lg font-semibold leading-snug container mx-auto max-w-2xl text-center flex-1">
-      <div className="py-12 px-3 md:p-20">
-        <p className="mb-5">
+      <div className="py-12 px-4 md:p-20">
+        <p className="mb-6">
           Milan Bargiel
           <br />
           Hauptstraße 36
@@ -15,30 +15,28 @@ export default function LegalNotice() {
         <Link
           href="mailto:milanbargiel@pm.me"
           variant="underline"
-          className="mb-5 inline-block"
+          className="mb-6 block"
         >
           milanbargiel@pm.me
         </Link>
-        <p className="mb-5">
+        <p className="mb-6">
           Data Protection Policy (GDPR)
           <br />
           This website does not collect, store or process Personally
           Identifiable Information (PII). This website does not require the use
           of cookies.
         </p>
-        <p className="mb-5">
+        <p className="mb-6">
           Third Party Inclusion
           <br />
           This website is hosted by Github using Github Pages. The Github Global
           Privacy Practices are available&nbsp;
-          <a
+          <Link
             href="https://docs.github.com/en/github/site-policy/global-privacy-practices"
-            target="_blank"
-            className="underline underline-offset-2 decoration-1 decoration-gray-700 hover:decoration-2 hover:transition-all hover:duration-150"
-            rel="noreferrer"
+            variant="underline"
           >
             here
-          </a>
+          </Link>
           .
         </p>
       </div>
@@ -51,7 +49,7 @@ LegalNotice.getLayout = function getLayout(page) {
   return (
     <Layout>
       {page}
-      <div className="text-center leading-tight mb-5">
+      <div className="text-center leading-tight mb-6">
         <Link
           href="/"
           className="font-semibold text-purple-500 hover:text-purple-700"
