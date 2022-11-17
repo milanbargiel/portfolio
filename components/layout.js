812482e0ad46e513faa08d0b2a638097f5e3a250
milanbargiel <milan.bargiel@googlemail.com>
2022-11-17 15:56:01 +0100

    Add open graph information for social sharing

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,16 @@ export default function Layout({ children }) {
           name="description"
           content="The Portfolio and Work Archive of Product Owner and Creative Technologist Milan Bargiel."
         />
+        <meta property="og:title" content="Milan Bargiel" />
+        <meta
+          property="og:description"
+          content="The Portfolio and Work Archive of Product Owner and Creative Technologist Milan Bargiel."
+        />
+        <meta
+          property="og:image"
+          content="https://www.milanbargiel.com/milanbargiel-webpage-og.jpg"
+        />
+        <meta property="og:url" content="https://www.milanbargiel.com/" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="px-4 container mx-auto min-h-screen flex flex-col md:px-8">
