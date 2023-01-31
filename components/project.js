85b7dca22738aba7ebf847ddd2cdf06466e7a8ab
Milan Bargiel <milan.bargiel@googlemail.com>
2023-01-31 17:21:20 +0100

    Refact project component

--- components/project.js
+++ components/project.js
@@ -1,18 +1,29 @@
 import Image from '../components/image'
 
 export default function Project(props) {
-  // Choose custom color for project card if specified
-  const backgroundColor = props.color ? props.color : '#f6f6f6'
+  // Object destructuring
+  const {
+    color,
+    roles,
+    className,
+    title,
+    image,
+    imageAlt,
+    imageStyles,
+    imageLink,
+    children,
+  } = props
 
-  // Create HTML li items from props.roles array
-  const roleItems = props.roles.map((role, index) => (
-    <li key={index}>{role}</li>
-  ))
+  // Choose custom color for project card if specified
+  const backgroundColor = color ? color : '#f6f6f6'
+
+  // Create HTML li items from roles array
+  const roleItems = roles.map((role, index) => <li key={index}>{role}</li>)
 
   return (
     <article
       className={`drop-shadow-sm px-4 pt-12 pb-4 mb-8 rounded-lg md:pt-24 md:px-8 md:mr-12 md:mb-12 lg:w-5/12 2xl:w-[26rem] md:relative ${
-        props.className ? props.className : ''
+        className ? className : ''
       }`}
       style={{ backgroundColor }}
     >
@@ -20,16 +31,16 @@ export default function Project(props) {
       <ul className="text-xs mb-8 md:leading-normal md:absolute md:top-10">
         {roleItems}
       </ul>
-      <h3 className="mb-8 text-2xl leading-tight">{props.title}</h3>
-      {props.image && (
+      <h3 className="mb-8 text-2xl leading-tight">{title}</h3>
+      {image && (
         <Image
-          src={props.image}
-          alt={props.imageAlt}
-          className={props.imageStyles}
-          href={props.imageLink ? props.imageLink : ''} // Add image link if there is one
+          src={image}
+          alt={imageAlt}
+          className={imageStyles}
+          href={imageLink ? imageLink : ''} // Add image link if there is one
         ></Image>
       )}
-      <div className="text-sm mb-8">{props.children}</div>
+      <div className="text-sm mb-8">{children}</div>
     </article>
   )
 }
