cebf4a065ae842387a9f628e717e7752e45a6de4
Milan Bargiel <milan.bargiel@googlemail.com>
2023-02-01 10:15:36 +0100

    Add text for BSE project

--- components/page-layout.js
+++ components/page-layout.js
@@ -1,9 +1,11 @@
 // A component for longer text layout
 import Header from './header'
 
-const PageLayout = ({ children }) => {
+const PageLayout = ({ children, className }) => {
   return (
-    <div className="my-12 md:mx-8 md:mb-20 md:mt-16 md:w-2/3 lg:w-3/5 xl:w-1/2 2xl:w-2/5">
+    <div
+      className={`my-12 md:mx-8 md:mb-20 md:mt-16 md:w-2/3 lg:w-3/5 xl:w-1/2 2xl:w-2/5 ${className}`}
+    >
       <Header />
       {children}
     </div>
--- pages/projects/building-social-ecology.js
+++ pages/projects/building-social-ecology.js
@@ -1,10 +1,39 @@
-import PageLayout from '../../components/page'
+import PageLayout from '../../components/page-layout'
+import Link from '../../components/link'
 
 export default function BuildingSocialEcology() {
   return (
     <>
       <PageLayout>
-        A living archive of socio-ecological building design patterns
+        <h1 className="text-3xl mb-8">
+          A living archive of socio-ecological design patterns
+        </h1>
+        <p className="mb-4 text-gray-300 text-sm">
+          Building Social Ecology showcases sustainable design patterns for
+          architects, residents and city developers in Europe.
+        </p>
+        <p className="mb-4 text-gray-300 text-sm">
+          In this DBU-funded (Deutsche Bundesstiftung Umwelt) research project I
+          helped a team from the University of Applied Sciences in Potsdam and a
+          cultural center in Bratislava with designing the projects website. We
+          worked together on on a new platform where they can publish and
+          archive research more easily.
+        </p>
+        <p className="mb-4 text-gray-300 text-sm">
+          My process included conducting workshops with the project partners,
+          collecting visual inspiration in a moodboard, building wireframes and
+          designing the interface of the application in Figma.
+        </p>
+        <p className="mb-4 text-gray-300 text-sm">
+          For the implementation I developed a custom theme in WordPress.
+        </p>
+        <Link
+          href="https://www.buildingsocialecology.org/"
+          className="text-gray-300 text-sm"
+          variant="underline-white"
+        >
+          www.buildingsocialecology.org
+        </Link>
       </PageLayout>
     </>
   )
