144c67f9666a63f11e8a0a0d030f3308647c1e6e
Milan Bargiel <milan.bargiel@googlemail.com>
2023-02-03 10:04:31 +0100

    Update texts

--- components/project-list.js
+++ components/project-list.js
@@ -8,52 +8,31 @@ export default function ProjectList() {
     <div>
       <div className="mb-24 text-black md:flex md:flex-wrap">
         <Project
-          roles={['Product Owner', 'Agile Methods']}
-          title="A lightweight, scalable database that stores more than 10.000 comments a day"
-        >
-          <p className="mb-4">
-            In 2020, the news website ZEIT ONLINE replaced its Drupal-based
-            comments database with a lightweight, flexible and scalable system
-            that stores more than 10.000 new comments a day.
-          </p>
-          <p className="mb-4">
-            As a product owner, I and my colleague Thomas Strotjohann set up an
-            agile development process and defined the new database’s qualities
-            in user stories.
-          </p>
-          <p className="mb-4">
-            The challenge was to exchange databases during live operations.
-            Working in the invisible and reconnecting a service deeply
-            intertwined within the basic functioning of the news web page was an
-            inspiring challenge.
-          </p>
-        </Project>
-        <Project
-          roles={['Interface Design', 'Development']}
-          title="An archive of socio-ecological design patterns for architects, city developers and residents"
-          image={bsePic}
-          imageAlt="Screenshot of buildingsocialecology.org"
-          imageStyles="rounded-md drop-shadow-sm border border-gray-700 hover:cursor-pointer"
-          imageLink="https://www.buildingsocialecology.org/"
-          color="#07D070"
-        ></Project>
-        <Project
           roles={['Product Owner', 'User Experience']}
           title="Reimagining a digital tool for the moderation of comments at ZEIT ONLINE"
         >
           <p className="mb-4">
-            The comment sections of articles are an essential feature of the
-            online news media. They allow readers to exchange ideas, engage in
-            discussions and react to what is going on in the world. To help the
-            professional moderators to do their job and keep comment sections a
-            safe space, a new tool was needed.
+            Comment sections are an essential feature of online news media{' '}
+            <Link
+              href="https://www.zeit.de/"
+              className="inline underline underline-offset-1"
+            >
+              www.zeit.de
+            </Link>
+            . They allow people to exchange ideas, engage in discussions and
+            express their thoughts about what is going on in the world.
+          </p>
+          <p className="mb-4">
+            To keep comment sections a safe space, the professional team of
+            moderators at ZEIT ONLINE needed a new tool that reduced the
+            complexity of former workflows and provides a seamless moderation
+            experience.
           </p>
           <p className="mb-4">
             In this project, I took the role of a product owner together with
-            the former head of product at ZEIT ONLINE. We aimed at building a
-            tool that provides a seamless moderation experience. By continous
-            user testing, we were able to give shape to the tool while actually
-            building it.
+            the former head of product at ZEIT ONLINE. By conducting workshops
+            with moderators, prototyping and continuous user testing, we were
+            able to give shape to the tool while actually building it.
           </p>
           <div className="mt-8">
             <Link
@@ -65,6 +44,37 @@ export default function ProjectList() {
             </Link>
           </div>
         </Project>
+        <Project
+          roles={['Interface Design', 'Development']}
+          title="An archive of socio-ecological design patterns for architects, city developers and residents"
+          image={bsePic}
+          imageAlt="Screenshot of buildingsocialecology.org"
+          imageStyles="rounded-md drop-shadow-sm border border-gray-700 hover:cursor-pointer"
+          imageLink="https://www.buildingsocialecology.org/"
+          color="#07D070"
+        ></Project>
+        <Project
+          roles={['Product Owner', 'API Development']}
+          title="A lightweight, scalable database that stores more than 10.000 comments a day"
+        >
+          <p className="mb-4">
+            In 2020, the news website ZEIT ONLINE replaced its comments database
+            with a lightweight, flexible and scalable system that stores more
+            than 10.000 new comments a day.
+          </p>
+          <p className="mb-4">
+            As a product owner, I and my colleague Thomas Strotjohann set up an
+            agile development process and defined the database’s qualities in
+            user stories.
+          </p>
+          <p className="mb-4">
+            The challenge was to exchange databases during live operations.
+            Working in the invisible and reconnecting a service deeply
+            intertwined with the basic functioning of the news web page was an
+            inspiring challenge.
+          </p>
+        </Project>
+
         <Project
           roles={['Co-Founder']}
           title="Kulturgenerator: A solidaric art gallery in times of quarantine and isolation"
