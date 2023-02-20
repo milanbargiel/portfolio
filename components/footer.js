034e97436bc17d743e1091a1515c89ab427f5cf9
Milan Bargiel <milan.bargiel@googlemail.com>
2023-02-20 15:59:33 +0100

    Simplify footer

--- components/footer.js
+++ components/footer.js
@@ -1,49 +1,33 @@
 import Link from './link'
 
 const Footer = ({ variant }) => {
-  let showBackLink, underlineVariant
+  let showBackLink, legalLinkClasses
 
   switch (variant) {
     case 'home':
-      underlineVariant = 'underline-white'
+      legalLinkClasses = 'font-semibold text-purple-700 hover:text-purple-500'
       break
     case 'project':
+      legalLinkClasses = 'text-xs text-gray-700 hover:text-gray-500'
       showBackLink = true
-      underlineVariant = 'underline-dark'
       break
   }
 
   return (
-    <>
-      <div className="text-center w-80 mx-auto">
-        <div className="text-[#B1AFDC] font-semibold mb-2">
-          Like what you see?
-        </div>
+    <div className="text-center w-80 mx-auto mb-6">
+      {/* Only show backlink for the project variant */}
+      {showBackLink && (
         <div className="mb-6 font-semibold leading-tight">
-          Let’s work together! Get in touch by{' '}
-          <Link
-            href="https://www.linkedin.com/in/milanbargiel"
-            variant={underlineVariant}
-          >
-            LinkedIn
-          </Link>{' '}
-          or by{' '}
-          <Link href="mailto:milanbargiel@pm.me" variant={underlineVariant}>
-            E-Mail
-          </Link>
-          .
-        </div>
-        {/* Only show backlink for the project variant */}
-        {showBackLink && (
           <Link href="/" className="text-[#0006F6]">
             (go back)
           </Link>
-        )}
-      </div>
-      <div className="text-center text-xs text-gray-500 mt-10 mb-1">
-        <Link href="/legal-notice">Imprint & Privacy</Link>
-      </div>
-    </>
+        </div>
+      )}
+      <Link className={legalLinkClasses} href="/legal-notice">
+        Legal notice &amp;
+        <br class="md:hidden" /> data privacy
+      </Link>
+    </div>
   )
 }
 
