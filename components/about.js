d98928aed1d6d04ea12a17197f084b2193e7cfd6
Milan Bargiel <milan.bargiel@googlemail.com>
2023-02-20 15:23:38 +0100

    Change intro text

--- components/about.js
+++ components/about.js
@@ -4,17 +4,17 @@ export default function About() {
   return (
     <div className="md:text-lg">
       <div className="mb-12 text-gray-300">
-        <h2 className="font-semibold mb-4 text-white">About</h2>I am a product
-        owner, web developer and designer living and working in Berlin, Germany.
-        Former editorial product developer at&nbsp;
+        <h2 className="font-semibold mb-4 text-white">About</h2>I am a creative
+        developer living and working in Berlin, Germany. Former editorial
+        product owner at{' '}
         <Link
           variant="underline-white"
-          href="https://www.zeit.de/autoren/B/Milan_Bargiel/indexhttps://www.zeit.de/autoren/B/Milan_Bargiel/index"
+          href="https://www.zeit.de/autoren/B/Milan_Bargiel/index"
         >
           ZEIT ONLINE
         </Link>
-        . I am interested in UX design, research and building digital things.
-        Find some selected projects below.
+        . I am interested in UX design, research and future technologies. Find
+        some selected projects below.
       </div>
       <h2 className="font-semibold mb-4">Contact</h2>
       <div className="text-gray-500 md:basis-2/4">Email</div>
