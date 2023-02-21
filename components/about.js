56b7b74347d7a718576e9c0e0a154eeca5817d3f
Milan Bargiel <milan.bargiel@googlemail.com>
2023-02-21 10:26:57 +0100

    Update texts

--- components/about.js
+++ components/about.js
@@ -4,17 +4,17 @@ export default function About() {
   return (
     <div className="md:text-lg">
       <div className="mb-12 text-gray-300">
-        <h2 className="font-semibold mb-4 text-white">About</h2>I am a creative
-        developer living and working in Berlin, Germany. Former editorial
-        product owner at{' '}
+        <h2 className="font-semibold mb-4 text-white">About</h2>Hello, I am
+        Milan Bargiel. I work in the design and development of websites and
+        digital products. Former editorial product owner at{' '}
         <Link
           variant="underline-white"
           href="https://www.zeit.de/autoren/B/Milan_Bargiel/index"
         >
           ZEIT ONLINE
         </Link>
-        . I am interested in UX design, research and future technologies. Find
-        some selected projects below.
+        . I am interested in UX design, research and web technologies. Find some
+        selected projects below.
       </div>
       <h2 className="font-semibold mb-4">Contact</h2>
       <div className="text-gray-500 md:basis-2/4">Email</div>
